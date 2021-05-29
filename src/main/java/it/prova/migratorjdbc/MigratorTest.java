b329de7198455e32742aaafe5a75efb1ed3e68b4
emanutri <emanuele.trisi@gmail.com>
2021-05-29 17:15:55 +0200

    added read from csv

--- src/main/java/it/prova/migratorjdbc/MigratorTest.java
+++ src/main/java/it/prova/migratorjdbc/MigratorTest.java
@@ -1,42 +1,69 @@
 package it.prova.migratorjdbc;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import it.prova.migratorjdbc.model.Assicurato;
 import it.prova.migratorjdbc.model.NotProcessed;
-import it.prova.migratorjdbc.model.OldSchema;
 import it.prova.migratorjdbc.service.AssicuratoService;
+import it.prova.migratorjdbc.service.CsvReaderServiceImpl;
 import it.prova.migratorjdbc.service.MyServiceFactory;
-import it.prova.migratorjdbc.service.OldSchemaService;
 
 public class MigratorTest {
 
 	public static void main(String[] args) throws Exception {
 
 		AssicuratoService assicuratoService = MyServiceFactory.getAssicuratoServiceImpl();
+//-------------DATA MIGRATOR FROM CSV-------------------
 
-		OldSchemaService oldSchemaService = MyServiceFactory.getOldSchemaServiceImpl();
+		List<Assicurato> assicuratiDaVecchioDb = CsvReaderServiceImpl.readCsv("C:/Users/NEW ROOM/Desktop/CsvAssiucrato.csv");
 
-		List<OldSchema> oldData = oldSchemaService.listAll();
-		
-		for(OldSchema dataItem : oldData) {
-			if(dataItem.getCodiceFiscale().length() != 16 || dataItem.getCognome() == null || dataItem.getNome() == null) {
-				NotProcessed notProcessedTemp = new NotProcessed();
-				notProcessedTemp.setCodiceFiscale(dataItem.getCodiceFiscale());
-				notProcessedTemp.setOldId(dataItem.getId().toString());
-				
-				assicuratoService.inserisciNotProcessed(notProcessedTemp);
-			}else {
-				Assicurato assicuratoTemp = new Assicurato();
-				assicuratoTemp.setNome(dataItem.getNome());
-				assicuratoTemp.setCognome(dataItem.getCognome());
-				assicuratoTemp.setCodiceFiscale(dataItem.getCodiceFiscale());
-				assicuratoTemp.setDataNascita(dataItem.getDataNascita());
-				assicuratoTemp.setNuoviSinistri(dataItem.getNumeroSinistri());
-				
-				assicuratoService.inserisciNuovo(assicuratoTemp);
+		List<Assicurato> assicurati = assicuratiDaVecchioDb.stream()
+				.filter(assicurato -> assicurato.getCodiceFiscale().length() == 16).collect(Collectors.toList());
+		List<Assicurato> assicuratiNotProcessed = assicuratiDaVecchioDb.stream()
+				.filter(assicurato -> assicurato.getCodiceFiscale().length() != 16).collect(Collectors.toList());
+
+		assicurati.stream().forEach(assicuratoItem -> {
+			try {
+				assicuratoService.inserisciNuovo(assicuratoItem);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		}
-		
+		});
+		assicuratiNotProcessed.stream().forEach(assicuratoNonProcessatoItem -> {
+			try {
+				NotProcessed notProcessedItem = new NotProcessed();
+				notProcessedItem.setCodiceFiscale(assicuratoNonProcessatoItem.getCodiceFiscale());
+				notProcessedItem.setOldId(assicuratoNonProcessatoItem.getId().toString());
+				assicuratoService.inserisciNotProcessed(notProcessedItem);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+
+//-------------DATA MIGRATO BETWEEN DB'S-------------------
+//		OldSchemaService oldSchemaService = MyServiceFactory.getOldSchemaServiceImpl();
+//
+//		List<OldSchema> oldData = oldSchemaService.listAll();
+//		
+//		for(OldSchema dataItem : oldData) {
+//			if(dataItem.getCodiceFiscale().length() != 16 || dataItem.getCognome() == null || dataItem.getNome() == null) {
+//				NotProcessed notProcessedTemp = new NotProcessed();
+//				notProcessedTemp.setCodiceFiscale(dataItem.getCodiceFiscale());
+//				notProcessedTemp.setOldId(dataItem.getId().toString());
+//				
+//				assicuratoService.inserisciNotProcessed(notProcessedTemp);
+//			}else {
+//				Assicurato assicuratoTemp = new Assicurato();
+//				assicuratoTemp.setNome(dataItem.getNome());
+//				assicuratoTemp.setCognome(dataItem.getCognome());
+//				assicuratoTemp.setCodiceFiscale(dataItem.getCodiceFiscale());
+//				assicuratoTemp.setDataNascita(dataItem.getDataNascita());
+//				assicuratoTemp.setNuoviSinistri(dataItem.getNumeroSinistri());
+//				
+//				assicuratoService.inserisciNuovo(assicuratoTemp);
+//			}
+//		}
+
 	}
 }
---
+++ src/main/java/it/prova/migratorjdbc/service/CsvReaderServiceImpl.java
@@ -0,0 +1,39 @@
+package it.prova.migratorjdbc.service;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.prova.migratorjdbc.model.Assicurato;
+
+public class CsvReaderServiceImpl{
+
+	public static List<Assicurato> readCsv(String filePath) throws IOException {
+		BufferedReader csvReader = new BufferedReader(new FileReader(filePath));
+		String row;
+		List<String[]> datiVecchioDb = new ArrayList<>();
+		while ((row = csvReader.readLine()) != null) {
+			String[] data = row.split(",");
+			datiVecchioDb.add(data);
+		}
+		csvReader.close();
+
+		List<Assicurato> assicuratiDaVecchioDb = new ArrayList<>();
+		for (int i = 0; i < datiVecchioDb.size(); i++) {
+			String[] assicuratoString = datiVecchioDb.get(i);
+			Assicurato assicurato = new Assicurato();
+			assicurato.setId(Integer.parseInt(assicuratoString[0]));
+			assicurato.setNome(assicuratoString[1]);
+			assicurato.setCognome(assicuratoString[2]);
+			assicurato.setCodiceFiscale(assicuratoString[3]);
+			assicurato.setDataNascita(Date.valueOf(assicuratoString[4]));
+			assicurato.setNuoviSinistri(Integer.valueOf(assicuratoString[5]));
+			assicuratiDaVecchioDb.add(assicurato);
+		}
+		return assicuratiDaVecchioDb;
+	}
+
+}
